39163a495ceb723f92b2dba15e6c5e95e681694b
papalino456 <sebastian.barrio.b@gmail.com>
2023-10-16 14:36:17 -0600

    e

--- main.py
+++ main.py
@@ -69,19 +69,19 @@ while True:
         motorBL.drive(2, 20)
         motorBR.drive(2, 20)
     elif not valL:  # If line is detected by the left sensor
-        # Move slightly to the right
-        motorFL.drive(2, 20)
+        # Move slightly tothe right
+        motorFL.drive(2, 15)
         motorFR.drive(1, 10)
-        motorBL.drive(2, 20)
+        motorBL.drive(2, 15)
         motorBR.drive(1, 10)
-        time.sleep(0.3)
+        time.sleep(0.2)
     elif not valR:  # If line is detected by the right sensor
         # Move slightly to the left
         motorFL.drive(1, 10)
-        motorFR.drive(2, 20)
+        motorFR.drive(2, 15)
         motorBL.drive(1, 10)
-        motorBR.drive(2, 20)
-        time.sleep(0.3)
+        motorBR.drive(2, 15)
+        time.sleep(0.2)
     else:  # If line is not detected
         # Stop
         motorFL.drive(2, 20)
