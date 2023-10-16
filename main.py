9477620c1eb110b41e410bdc94d411381b4010c0
papalino456 <sebastian.barrio.b@gmail.com>
2023-10-16 13:42:06 -0600

    changedir2

--- main.py
+++ main.py
@@ -70,16 +70,16 @@ while True:
         motorBR.drive(2, 100)
     elif not valL:  # If line is detected by the left sensor
         # Move slightly to the right
-        motorFL.drive(2, 50)
-        motorFR.drive(2, 100)
-        motorBL.drive(2, 50)
-        motorBR.drive(2, 100)
-    elif not valR:  # If line is detected by the right sensor
-        # Move slightly to the left
         motorFL.drive(2, 100)
         motorFR.drive(2, 50)
         motorBL.drive(2, 100)
         motorBR.drive(2, 50)
+    elif not valR:  # If line is detected by the right sensor
+        # Move slightly to the left
+        motorFL.drive(2, 50)
+        motorFR.drive(2, 100)
+        motorBL.drive(2, 50)
+        motorBR.drive(2, 100)
     else:  # If line is not detected
         # Stop
         motorFL.stop()
