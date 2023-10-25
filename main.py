214771f098decf9ff464948057e8062b65f000c5
papalino456 <sebastian.barrio.b@gmail.com>
2023-10-25 15:32:13 -0600

    30pot

--- main.py
+++ main.py
@@ -86,10 +86,10 @@ while True:
         motorFL.stop()
     elif not valC:  # If line is detected by the center sensor
         # Move forward
-        motorFL.drive(2, 25)
-        motorFR.drive(2, 25)
-        motorBL.drive(2, 25)
-        motorBR.drive(2, 25)
+        motorFL.drive(2, 30)
+        motorFR.drive(2, 30)
+        motorBL.drive(2, 30)
+        motorBR.drive(2, 30)
     elif not valL:  # If line is detected by the left sensor
         # Move slightly tothe right
         motorFL.drive(2, 25)
@@ -121,10 +121,10 @@ while True:
     """
     else:  # If line is not detected
         # Stop
-        motorFL.drive(2, 25)
-        motorFR.drive(2, 25)
-        motorBL.drive(2, 25)
-        motorBR.drive(2, 25)
+        motorFL.drive(2, 30)
+        motorFR.drive(2, 30)
+        motorBL.drive(2, 30)
+        motorBR.drive(2, 30)
 
 
 
