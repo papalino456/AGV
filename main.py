2f7673c5522de9770af064d2a26f89a554273879
papalino456 <sebastian.barrio.b@gmail.com>
2023-10-26 15:35:42 -0600

    25turn

--- main.py
+++ main.py
@@ -98,17 +98,17 @@ while True:
         motorBR.drive(2, 20)
     elif not valL:  # If line is detected by the left sensor
         # Move slightly tothe right
-        motorFL.drive(2, 20)
-        motorFR.drive(1, 20)
-        motorBL.drive(2, 20)
-        motorBR.drive(1, 20)
+        motorFL.drive(2, 25)
+        motorFR.drive(1, 25)
+        motorBL.drive(2, 25)
+        motorBR.drive(1, 25)
         time.sleep(0.2)
     elif not valR:  # If line is detected by the right sensor
         # Move slightly to the left
-        motorFL.drive(1, 20)
-        motorFR.drive(2, 20)
-        motorBL.drive(1, 20)
-        motorBR.drive(2, 20)
+        motorFL.drive(1, 25)
+        motorFR.drive(2, 25)
+        motorBL.drive(1, 25)
+        motorBR.drive(2, 25)
         time.sleep(0.2)
         """
     elif not valFL:
