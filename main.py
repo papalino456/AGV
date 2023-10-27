81d289d7242a5298c4ee69fb376d575b7b88193b
papalino456 <sebastian.barrio.b@gmail.com>
2023-10-27 14:09:07 -0600

    30 juas juas

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ while True:
         time.sleep(0.05)
     else:  # If line is not detected
         # Stop
-        motorFL.drive(2, 50)
-        motorFR.drive(2, 50)
-        motorBL.drive(2, 50)
-        motorBR.drive(2, 50)
+        motorFL.drive(2, 30)
+        motorFR.drive(2, 30)
+        motorBL.drive(2, 30)
+        motorBR.drive(2, 30)
