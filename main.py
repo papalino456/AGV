8ee13a621a9886ea80374d79e3b1893154d78ebf
papalino456 <sebastian.barrio.b@gmail.com>
2023-10-27 12:19:51 -0600

    25

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ while True:
         time.sleep(0.1)
     else:  # If line is not detected
         # Stop
-        motorFL.drive(2, 20)
-        motorFR.drive(2, 20)
-        motorBL.drive(2, 20)
-        motorBR.drive(2, 20)
+        motorFL.drive(2, 25)
+        motorFR.drive(2, 25)
+        motorBL.drive(2, 25)
+        motorBR.drive(2, 25)
